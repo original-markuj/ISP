9fd78e11f2038488a0b6ff4d254bf3100ef95563
marku <marku@DESKTOP-EDVBC84>
2026-09-12 14:29:40 +0200

    Добавьте файлы проекта.

---
+++ C# first lesson (free)/Program.cs
@@ -0,0 +1,47 @@
+﻿//Console.WriteLine("Hello, I'm Falsity. Say your name, foolish men");
+
+//string name = Console.ReadLine();
+
+//if (name == "Den")
+//{
+//    Console.WriteLine($"Sorry {name}, I'm Lord Verity");
+//}
+//else
+//{
+//    Console.WriteLine($"I fooled you {name}, I'm Lord Verity");
+//}
+//Console.Write("Write X ");
+
+//double x = Convert.ToInt32(Console.ReadLine());
+
+//Console.Write("Write Y ");
+
+//double y = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine($"{x} + {y} = {x + y}");
+
+//if (x)
+//{
+//    Console.WriteLine($"{x} + {y} = {x + y}");
+//}
+
+//double x;
+//float a = 1;
+//float b = -1;
+//float c = -2;
+
+//x = ((-b - Math.Sqrt(Math.Pow(b,2) - 4*a*c))/(2*a));
+
+//Console.WriteLine(x);
+
+float x = 2;
+
+Console.Write($"Enter exp for {x}: ");
+
+double exp = Convert.ToDouble(Console.ReadLine());
+
+double y = Math.Exp(x * exp);
+
+Console.WriteLine($"Your exp for {x} is: {y}");
+
+Console.WriteLine($"Do not go home until you finish reading the value of e. {Math.E:f50}");
---
+++ Lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿//Console.Write("Enter x: ");
+//double Fx = Convert.ToDouble(Console.ReadLine());
+//double Fk = (Math.Sqrt(Math.Pow(3 + Fx, 6) - Math.Log(Fx))) / (Math.Exp(0) + Math.Asin(6 * Fx * Fx));
+//Console.WriteLine($"k = {Fk:f5}");
+
+//Console.Write("Enter x: ");
+//double Sx = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Enter a: ");
+//double Sa = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Enter c: ");
+//double Sc = Convert.ToDouble(Console.ReadLine());
+//double L = (Math.Sqrt(Math.Pow(Math.E, Sx) - Math.Pow(Math.Cos(Math.Pow(Sx,2) * Math.Pow(Sa,5)), 4)) + Math.Pow(Math.Atan(Sa - Math.Pow(Sx,5)),4)) / (Math.Exp(Math.Sqrt(Math.Abs(Sa + Sx * Math.Pow(Sc,4)))));
+
+//Console.WriteLine($"L = {L:f5}");
+
+//Console.Write("Enter y: ");
+//double y = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Enter h: ");
+//double h = Convert.ToDouble(Console.ReadLine());
+//double A = (Math.Tan(Math.Pow(y,3) - Math.Pow(h,4)) + Math.Pow(h,2)) / (Math.Pow(Math.Sin(h + y),3));
+
+//Console.WriteLine($"A = {A:f5}");
+
+
+//Вариант 8
+
+Console.Write("Enter x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter c: ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+
+double P = (Math.Pow(a, 5) + Math.Pow(Math.Sin(y - c), 4)) / (Math.Pow(Math.Sin(x + y), 3) + Math.Abs(x - y));
+Console.WriteLine($"P = {P:f5}");
